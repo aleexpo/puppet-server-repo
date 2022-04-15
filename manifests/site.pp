cba55a63b1fde7201a9c6c5874bdf8d6fc1bdf4b
aleexpo <alexerik1239@gmail.com>
2022-04-16 02:32:13 +0300

    new files added
    by @aleexpo

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,37 @@
 
 node default {
-file { '/root/README':
-ensure => file, }
+   file { '/root/README':
+   ensure => file, }
 }
+
+node slave1 {
+  package { 'nginx' :
+  ensure => installed,
+   }
+
+  -> file { '/etc/nginx/sites-available/static':
+   ensure => file, 
+   source => 'puppet:///modules/static/servconf/config',
+   }
+
+  -> file { '/var/www/sites/static/index.html':
+   ensure => directory,
+   source => 'puppet:///modules/static/index.html',
+   recurse => true,
+   }
+
+  -> file { '/etc/nginx/sites-available/static':
+   ensure => 'link',
+   target => '/etc/nginx/sites-enabled/',
+   }
+
+  ~> service { 'nginx':
+   ensure => running,
+   enable => true,
+   }
+}
+
+#node slave2 {
+   #package {'nginx', 'php-fpm', 'php-mysql', 'php':
+   #
+   #}
