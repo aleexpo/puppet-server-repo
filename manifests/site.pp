007c492ef6a542394993627e7837dd725e1cbed3
aleexpo <alexerik1239@gmail.com>
2022-04-16 18:52:00 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -7,8 +7,8 @@ node 'slave1' {
    source => 'puppet:///modules/static/index.html',
    recurse => true,
   }
-  include nginx
-  nginx::resource::server { 'static':
+  -> class{'nginx': }
+  -> nginx::resource::server { 'static':
    listen_port => 80,
    proxy       => '192.168.56.9:80',
    www_root => '/var/www/static/',
