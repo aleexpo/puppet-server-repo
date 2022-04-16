7b39bb351ed97ef5abce41564299a27e09e31294
aleexpo <alexerik1239@gmail.com>
2022-04-16 17:09:05 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,32 +1,20 @@
-
-
-
-node 'slave2' {
+node 'slave1' {
   file { '/root/README':
    ensure => file, 
   }
-
-node 'slave1' {
-   file { '/root/README':
-   ensure => file, 
-  }
-
   file { '/var/www/sites/static/index.html':
    ensure => file,
    source => 'puppet:///modules/static/index.html',
    recurse => true,
   }
-
-   file { '/root/README':
+  file { '/root/README':
    ensure => absent, 
   }
-
-
-  class{'nginx': }
-
+  class{'nginx':
+  }
   nginx::resource::server { 'mystaticsite':
-  listen_port => 80,
-  proxy       => '192.168.56.9:80',
-  www_root => '/var/www/static/',
+   listen_port => 80,
+   proxy       => '192.168.56.9:80',
+   www_root => '/var/www/static/',
   }
 }
