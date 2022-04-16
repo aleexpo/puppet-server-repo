5fade2ee9a32b5c56056c6c92909aa8b46602094
aleexpo <alexerik1239@gmail.com>
2022-04-16 18:43:33 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -8,7 +8,7 @@ node 'slave1' {
    recurse => true,
   }
   include nginx
-  nginx::resource::server { 'mystaticsite':
+  nginx::resource::server { 'static':
    listen_port => 80,
    proxy       => '192.168.56.9:80',
    www_root => '/var/www/static/',
