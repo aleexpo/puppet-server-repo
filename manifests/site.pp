f8043379eb9e76fa146ac760d32876f9737668d6
aleexpo <alexerik1239@gmail.com>
2022-04-18 21:35:42 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -67,7 +67,7 @@ node 'mineserver.puppet'{
   package { 'openjdk-8-jre-headless': }
   
   include wget
-    wget::fetch { 'https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar':
-    destination => '/opt/minecraft',
-    }
-}  
+  wget::retrieve { 'https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar':
+  destination => '/opt/minecraft',
+  }
+} 
