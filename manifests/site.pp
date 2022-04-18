a9aa41d227f26b98d2808c553a342eb4d0b42b03
aleexpo <alexerik1239@gmail.com>
2022-04-18 22:09:59 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -70,13 +70,13 @@ node 'mineserver.puppet'{
   file {'/opt/minecraft':
   ensure => directory,
   }
-  
+
   include wget
   wget::retrieve { 'https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar':
   destination => '/opt/minecraft',
   }
 
-  file { '/opt/minecraft':
+  file { '/opt/minecraft/eula.txt':
    ensure => 'file',
    source => 'puppet:///modules/minecraft/eula.txt'
   }
