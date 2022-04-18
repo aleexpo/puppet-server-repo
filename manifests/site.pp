3bfd2bb508d4cbeed6028254eb060bbfb6a7bcde
aleexpo <alexerik1239@gmail.com>
2022-04-18 22:52:19 +0300

    changes in module

--- manifests/site.pp
+++ manifests/site.pp
@@ -21,6 +21,7 @@ node 'slave2' {
   file { '/root/README':
    ensure => 'absent', 
   }
+  
   class { 'apache': }
   Package { ensure => 'installed' }
   package { 'php': }
@@ -63,31 +64,5 @@ node 'master.puppet' {
 
 
 node 'mineserver.puppet'{
-  Package { ensure => 'installed' }
-  package { 'default-jre': }
-  package { 'default-jdk': }
-  
-  file {'/opt/minecraft':
-  ensure => directory,
-  }
-
-  include wget
-  wget::retrieve { 'https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar':
-  destination => '/opt/minecraft/',
-  }
-
-  file { '/etc/systemd/system/minecraft.service':
-   ensure => 'file',
-   source => 'puppet:///modules/minecraft/service'
-  }
-
-  file { '/opt/minecraft/eula.txt':
-   ensure => 'file',
-   source => 'puppet:///modules/minecraft/eula.txt'
-  } 
-
-  service { 'minecraft.service':
-  ensure => running,
-  enable => true,
-  }
+ include minecraft auto-inst
 } 
---
+++ modules/minecraft/manifests/init.pp
@@ -0,0 +1,29 @@
+class minecraft auto-inst {
+  Package { ensure => 'installed' }
+  package { 'default-jre': }
+  package { 'default-jdk': }
+  
+  file {'/opt/minecraft':
+  ensure => directory,
+  }
+
+  include wget
+  wget::retrieve { 'https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar':
+  destination => '/opt/minecraft/',
+  }
+
+  file { '/etc/systemd/system/minecraft.service':
+   ensure => 'file',
+   source => 'puppet:///modules/minecraft/service'
+  }
+
+  file { '/opt/minecraft/eula.txt':
+   ensure => 'file',
+   source => 'puppet:///modules/minecraft/eula.txt'
+  } 
+
+  service { 'minecraft.service':
+  ensure => running,
+  enable => true,
+  }
+}
