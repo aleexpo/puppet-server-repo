344a7eb3c27a69d220c74e3b4c6fe8536b721854
aleexpo <alexerik1239@gmail.com>
2022-04-18 14:19:09 +0300

    more changes

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,18 +1,72 @@
 node 'slave1' {
   file { '/root/README':
-   ensure => file, 
+   ensure => 'absent', 
   }
-  file { '/usr/share/nginx/html/static':
-  ensure => directory,
-  }
-  file {'/usr/share/nginx/html/static/index.html':
-  ensure => file,
+
+  class { 'apache': }
+
+  -> file {'/var/www/html/index.html':
+  ensure => 'file',
   source => 'puppet:///modules/static/index.html',
   }
-  class{'nginx': }
-  nginx::resource::server { 'static':
-   listen_port => 80,
-   proxy       => 'http://192.168.56.9:80',
-   www_root => '/usr/share/nginx/html/static',
+
+  -> apache::vhost { 'static':
+  port          => '80',
+  docroot       => '/var/www/html',
+  }
+
+  ~> service {'apache2':
+    ensure => running, 
+    enable => true,
   }
 }
+
+node 'slave2' {
+  file { '/root/README':
+   ensure => 'absent', 
+  }
+  class { 'apache': }
+  Package { ensure => 'installed' }
+  package { 'php': }
+  package { 'libapache2-mod-php': }
+  package { 'php-mysql': }
+  package { 'mariadb-server': }
+  
+  -> file { '/var/www/php':
+  ensure => 'directory',
+  }
+
+  -> file {'/var/www/php/index.html':
+  ensure => 'file',
+  source => 'puppet:///modules/dynamic/index.php',
+  }
+
+  -> apache::vhost { 'dynamic':
+  port          => '81',
+  docroot       => '/var/www/php',
+  }
+
+  ~> service {'apache2':
+    ensure => running, 
+    enable => true
+  }
+}
+
+node 'master.puppet' {
+  include nginx
+  nginx::resource::server { 'slave1':
+   listen_port => 80,
+   proxy       => 'http://192.168.56.9:80',
+   www_root => '/var/www/html/',
+  }
+  nginx::resource::server { 'slave2':
+   listen_port => 81,
+   proxy       => 'http://192.168.56.10:81',
+   www_root => '/var/www/php',
+  }
+  ~> service {'nginx':
+    ensure => running, 
+    enable => true
+  }
+}
+
