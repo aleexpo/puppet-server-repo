e6e08076b3d29c127c406b1e4cafd9c9c94ccd69
aleexpo <101505832+aleexpo@users.noreply.github.com>
2022-04-21 20:43:31 +0300

    update port slave2

--- manifests/site.pp
+++ manifests/site.pp
@@ -39,7 +39,7 @@ node 'slave2' {
   }
 
   -> apache::vhost { 'dynamic':
-  port          => '81',
+  port          => '80',
   docroot       => '/var/www/php',
   }
 
@@ -57,7 +57,7 @@ node 'master.puppet' {
    proxy       => 'http://192.168.56.9:80',
   }
   nginx::resource::server { 'dynamic':
-   listen_port => 81,
+   listen_port => 80,
    proxy       => 'http://192.168.56.10:81',
   }
 }
